d0df5d1de0bee1b8495f9353293338ee2e9d273b
dnaport22 <nav8699@gmail.com>
2016-08-08 10:23:20 +0100

    Side panel improved

--- susApp/www/js/DrawerLinks.js
+++ susApp/www/js/DrawerLinks.js
@@ -3,7 +3,7 @@ angular.module('DrawerLinks', [])
 .controller('DrawerController', function($state, $scope, $location, $ionicLoading, $window, $ionicHistory, $timeout, $localStorage) {
 
 
-  $scope.username = 'Login to view more options';
+  $scope.username = localStorage.getItem('user_username');
   $scope.authTitle = [];
   $scope.authStatus = null;
   $scope.links = [];
@@ -14,50 +14,32 @@ angular.module('DrawerLinks', [])
     user_login_id: 0
   })
 
-  $scope.drawerLinks_top = [
-    {title: 'Home', class: 'icon ion-home', href: '#/app/main', id: 1},
-    {title: 'My items', class: 'icon ion-folder', href: '#/app/myitems', id: 1},
-    {title: 'About us', class: 'icon ion-information-circled', href: '#/app/aboutus', id: 1},
-    {title: 'Terms and conditions', class: 'icon ion-document-text', href: '#/app/terms', id: 1},
+  $scope.drawerLinks_loggedOut = [
     {title: 'Home', class: 'icon ion-home', href: '#/app/main', id: 0},
     {title: 'About us', class: 'icon ion-information-circled', href: '#/app/aboutus', id: 0}
   ];
 
-  $scope.drawerLinks_bottom = [
-    {title: 'Login', href: '#/app/login', id: 0},
-    {title: 'Logout', id: 1}
+  $scope.drawerLinks_loggedIn = [
+    {title: 'Home', class: 'icon ion-home', href: '#/app/main', id: 1},
+    {title: 'My items', class: 'icon ion-folder', href: '#/app/myitems', id: 1},
+    {title: 'About us', class: 'icon ion-information-circled', href: '#/app/aboutus', id: 1},
+    {title: 'Terms and conditions', class: 'icon ion-document-text', href: '#/app/terms', id: 1},
   ];
 
-  if (localStorage.getItem('user_status') == 1) {
-    $scope.authTitle = 'Logout';
-    $scope.authStatus = 1;
-    $scope.username = 'Logged in as ' + localStorage.getItem('user_username');
-    $scope.links.push(
-      {title: 'Home', class: 'icon ion-home', href: '#/app/main'},
-      {title: 'My items', class: 'icon ion-folder', href: '#/app/myitems'},
-      {title: 'About us', class: 'icon ion-information-circled', href: '#/app/aboutus'},
-      {title: 'Terms and conditions', class: 'icon ion-document-text', href: '#/app/terms'}
-    );
-  }
-  else {
-    $scope.authStatus = 0;
-    $scope.authTitle = 'Login';
-    $scope.links.push(
-      {title: 'Home', class: 'icon ion-home', href: '#/app/main'},
-      {title: 'About us', class: 'icon ion-information-circled', href: '#/app/aboutus'}
-    )
-  }
-
-  $scope.authAction = function(action) {
-    if (action == 'Login') {
-      $location.path('/app/login/main')
-    }
-    else if (action == 'Logout') {
+  $scope.logout = function() {
+    $ionicLoading.show({
+      content: 'Logging in',
+      animation: 'fade-in',
+      showBackdrop: true,
+      maxWidth: 200,
+      showDelay: 0
+    });
+    $timeout(function() {
       localStorage.setItem('user_username', '');
       localStorage.setItem('user_email', '');
       localStorage.setItem('user_activation', '');
       $localStorage.user_login_id = 0;
-
-    }
+    }, 10000)
+    $ionicLoading.hide();
   }
 });
