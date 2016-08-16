b0ebe1733b897a7a57b1fc1bea722b58d266f7ba
dnaport22 <nav8699@gmail.com>
2016-08-16 14:55:45 +0100

    LSA-93

--- backend/public_html/activation.php
+++ backend/public_html/activation.php
@@ -9,12 +9,12 @@ $code = $_GET['uniqueId'];
 $user = new User($mysql_db);
 if ($user->loadByCode($code)) {
 	if ($user->isActive()) {
-		Response::flush(1, 'Your user account was already active');
+		Response::flush(1, 'Your user account has already been activated.');
 	}
 	else {
 		$user->setStatus(1);
 		$user->save();
-		Response::flush(1, 'Thank you for activating your account, You are now now ready to use Sustainability app.');
+		Response::flush(1, 'Thank you for activating your account. You are now ready to start using the elephant app.');
 	}
 }
 Response::flush(0, 'Wrong Activation Code');
