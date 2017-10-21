6bba8943551cc763f41c000b2def4695231a27fc
deep-nav <navdeep.dhuti@cameronandwilding.com>
2017-10-21 16:08:00 +0100

    Adding search module

---
+++ www/js/Components/Search/Search.module.js
@@ -0,0 +1,17 @@
+var Search = angular.module('Search', ['d7-services'])
+	.config(function($stateProvider, $urlRouterProvider) {
+		const TEMPLATE_DIR = 'js/Components/';
+		$stateProvider
+			.state('app.login', {
+				url: '/search',
+				params: {
+					type: null
+				},
+				views: {
+					'menuContent': {
+						templateUrl: TEMPLATE_DIR + 'Search/src/Template/search.html',
+						controller: 'SearchViewController'
+					}
+				}
+			})
+	})
